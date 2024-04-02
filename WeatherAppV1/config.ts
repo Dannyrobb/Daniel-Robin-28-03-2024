8f89c45f6d86dca0c3e55df67cff823a978846ce
Dannyrobb <dannyrobb1997@gmail.com>
2024-04-02 16:21:20 +0300

    http>https

--- WeatherAppV1/config.ts
+++ WeatherAppV1/config.ts
@@ -1,6 +1,6 @@
 //DISCLAIMER - KEPT USUALLY ON SECURED SERVER SIDE
 export const API_KEY = "ElfrWWt8g7PMUVwSux9TG08HBxRlZE66";
-export const BASE_URL_LOCATIONS = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete";
-export const BASE_URL_CURRENT_WEATHER = "http://dataservice.accuweather.com/currentconditions/v1";
-export const BASE_URL_FIVE_DAY_FORECAST = `http://dataservice.accuweather.com/forecasts/v1/daily/5day`;
-export const BASE_URL_GEOLOCATION = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search`;
+export const BASE_URL_LOCATIONS = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete";
+export const BASE_URL_CURRENT_WEATHER = "https://dataservice.accuweather.com/currentconditions/v1";
+export const BASE_URL_FIVE_DAY_FORECAST = `https://dataservice.accuweather.com/forecasts/v1/daily/5day`;
+export const BASE_URL_GEOLOCATION = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search`;
